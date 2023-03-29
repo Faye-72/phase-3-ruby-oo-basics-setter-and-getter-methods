20875326cc15946089b3fc7b356899fd6d885554
fmitei <faith.mitei@trustgro.com>
2023-03-30 00:00:16 +0300

    submited assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-
+  #setter method
+    def name=(name)
+        @name = name
+    end
+    #getter method
+    def name
+        @name
+    end
+    #setter
+    def breed=(breed)
+        @breed= breed
+    end
+    #getter
+    def breed
+        @breed
+    end
 end
